85d66c14664b8f829bf48df2a4bacc5624496812
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2018-04-25 16:05:56 +0100

    Update regular meting so dates update automatically

--- page-templates/meetings-page.php
+++ page-templates/meetings-page.php
@@ -50,7 +50,55 @@ Template Name: Regular Meetings Page
 				<?php foreach ($regular_meetings as $rm) { 
 				$meeting_img = $rm['img'];
 				$meeting_title = $rm['title'];
+				$date_type = $rm['date_type'];
 				$frequency = $rm['frequency'];
+				
+				if ($date_type == 'repeat') {
+				$frequency = $rm['frequency'];	
+				$day = $rm['day'];	
+				$week = $rm['week'];
+				$end_day = $day[count($day) - 1];
+				$end_week = $week[count($week) - 1];
+				$dates = array();
+				$now = new dateTime();
+				$now_date = $now->format('Ymd');
+				$now_day = $now->format('l');
+				$now_mth = $now->format('F');	
+				$now_yr = $now->format('Y');
+				$interval = new DateInterval('P1M');
+				
+				if ($end_week != 'All') {
+					$end_mt_date = date('Ymd', strtotime($end_week.' '.$end_day.' of '.$now_mth));	
+				} else {	
+					$end_mt_date = date('Ymd', strtotime('Last '.$end_day.' of '.$now_mth));
+					
+					if (weeks($now->format('d'), $now_yr) > 4 ) {
+					$week = array("first", "second", "third", "fourth", "last");
+					}	
+					
+					if (weeks($now->format('d'), $now_yr) > 5 ) {
+					$week[4] = "fifth";
+					$week[] = "last";
+					}	
+				}
+				
+				if ( $now_date > $end_mt_date ) {
+					$now->add($interval);
+					$now_mth = $now->format('F');	
+				} else {
+					$now_mth = $now->format('F');	
+				}
+
+				foreach ($week as $k => $wk) { 
+					foreach ($day as $dk => $dy) {
+					$dates[] = date('F jS', strtotime($wk.' '.$dy.' of '.$now->format('F').' '.$now_yr));		
+					}
+				}	
+				
+				} else {
+				$meeting_date = $rm['date'];	
+				}
+				
 				$meeting_date = $rm['date'];
 				$meeting_time = $rm['time'];
 				$meeting_venue = $rm['venue'];
@@ -64,9 +112,14 @@ Template Name: Regular Meetings Page
 				<div class="meeting-details">
 					<div class="meeting-img" style="background-image: url(<?php echo $feat_img[0]; ?>)"></div>
 					<div class="title"><?php echo $meeting_title; ?></div>
-					<div class="frequency"><?php echo $frequency; ?></div>
-					<div class="date">Date: <?php echo $meeting_date; ?></div>
-					<div class="time">Time: <?php echo $meeting_time; ?></div>	
+					<?php if ($date_type == 'repeat') { ?>
+					<div class="frequency"><?php echo $frequency; ?> <?php echo $now_yr; ?></div>	
+					<div class="date">Date(s):<br><?php echo implode('<br>', $dates); ?></div>
+					<div class="time">Time: <?php echo $meeting_time; ?></div>			
+					<?php } ?>
+					<?php if ($date_type == 'single') { ?>
+					<div class="date">Date: <?php echo $meeting_date; ?> @ <?php echo $meeting_time; ?></div>
+					<?php } ?>
 					<div class="venue">Venue: <?php echo $meeting_venue; ?></div>	
 					<div class="desc"><?php echo $meeting_desc; ?></div>
 					<div class="contact">Contact: <?php echo $meeting_contact_name; ?><br>
